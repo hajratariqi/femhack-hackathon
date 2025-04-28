f7852740a30e10eb6d5d9fd68336a4e60fbbcfa0
hajratariqi <hajra.ashrafqadri@gmail.com>
2025-04-29 03:28:40 +0500

    new changes done

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { LogOut, LogIn } from 'lucide-react';
+import { LogOut, LogIn, ArrowRight } from 'lucide-react';
+import { Link } from 'react-router-dom';
 
 const Header = ({user}) => {
   const logout = () => {
@@ -13,8 +14,16 @@ const Header = ({user}) => {
         {user ? (
         <button onClick={logout} className="flex items-center gap-2 bg-[#4e342e] text-white font-semibold px-5 py-2 rounded-lg hover:bg-[#6d4c41] transition-all duration-300"><LogOut size={20} /> Logout</button>
       ):(
-        <button onClick={logout} className="flex items-center gap-2 bg-[#4e342e] text-white font-semibold px-5 py-2 rounded-lg hover:bg-[#6d4c41] transition-all duration-300"><LogIn  size={20} /> Get started</button> 
+        <>
+           <Link to="/login">
+                    <button type="submit" className="group flex justify-center items-center gap-2 bg-gradient-to-r px-8 cursor-pointer from-amber-600 to-amber-800 text-white font-bold text-lg py-4 rounded-xl hover:scale-105 hover:shadow-lg transition-all duration-300 transform hover:-translate-y-1">
+                      <span>Get Started</span>
+                      <ArrowRight size={20} className="group-hover:translate-x-1 transition-transform duration-300" />
+                    </button>
+          </Link>
+       </>
       )}
+      
       </div>
     </header>
   );
